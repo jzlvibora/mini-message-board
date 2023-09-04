0d2f422eb7a82895f5db514a8662bee3bfad0d34
Jayzel Gonzales <j.vbr.gonzales@gmail.com>
2023-09-04 22:24:55 +0800

    added new message route

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,37 @@ const messages = [
     text: "Hello World!",
     user: "Charles",
     added: new Date()
+  },
+  {
+    text: "Happy Coding!",
+    user: "Lorem",
+    added: new Date()
+  },
+  {
+    text: "Coding is Fun",
+    user: "Ipsum",
+    added: new Date()
   }
 ];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Coding is Fun' , messageList: messages});
+  res.render('index', { title: 'Mini Message Board' , messageList: messages});
 });
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'Mini MessageBoard' });
+});
+
+router.post('/new', function(req,res,next) {
+  console.log(req.body.author)
+  console.log(req.body.message)
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date()
+  })
+  res.redirect('/')
+})
 
 module.exports = router;
